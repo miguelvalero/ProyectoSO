aae8219696a2705c0aa6ec9fb89e189bf264581a
Miguel Valero <miguel.valero@upc.edu>
2019-10-10 16:34:01 +0200

    Nombre en rojo

--- cliente_inicial/WindowsFormsApplication1/Form1.Designer.cs
+++ cliente_inicial/WindowsFormsApplication1/Form1.Designer.cs
@@ -41,6 +41,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
             this.label2.Location = new System.Drawing.Point(23, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(104, 25);
